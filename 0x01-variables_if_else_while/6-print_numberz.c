2da7ed3664692368fcc9774d345897388ea73f88
kindong96 <kindong96@gmail.com>
2022-03-22 11:47:48 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,18 @@
- #include <stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
+
+/* main fonction is void */
 int main(void)
 {
-	int i;
-	long int i2;
-	long long int i3;
-	char c;
-	float f;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(i2));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(i3));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	for (int i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
 	return (0);
 }
