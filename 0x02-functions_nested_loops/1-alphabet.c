7c34a7dff0ed11e2582e3d016967908c332070ef
kindong96 <kindong96@gmail.com>
2022-03-28 14:15:55 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+void print_alphabet(void);
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet lowercase
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+_putchar('\n');
+
+}
